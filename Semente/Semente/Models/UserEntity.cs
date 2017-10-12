9d745db0cf55ff9b47883d11ed8e836d5b3599e1
1150524 <1150524@isep.ipp.pt>
2017-10-12 11:32:29 +0100

    Tutoriau numero dois

---
+++ Semente/Semente/Models/UserEntity.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Semente.Models
+{
+    public class UserEntity : IdentityUser
+    {
+    }
+}
